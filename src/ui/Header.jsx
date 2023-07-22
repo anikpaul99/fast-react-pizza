1d45961c66f3206b73a2948937b61e2d95e00f56
Anik Paul <132483203+anikpaul99@users.noreply.github.com>
2023-07-22 19:03:27 +0600

    :sparkles: feat: add the app layout

---
+++ src/ui/Header.jsx
@@ -0,0 +1,13 @@
+import { Link } from "react-router-dom";
+
+function Header() {
+  return (
+    <header>
+      <Link to="/">Fast React Pizza Co.</Link>
+
+      <p>Anik</p>
+    </header>
+  );
+}
+
+export default Header;

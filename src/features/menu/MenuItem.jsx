77eead28c0ab1dce5f57f1743eabf3b05b4f27f0
Anik Paul <132483203+anikpaul99@users.noreply.github.com>
2023-07-27 10:44:42 +0600

    :sparkles: feat: add menu item to cart

--- src/features/menu/MenuItem.jsx
+++ src/features/menu/MenuItem.jsx
@@ -1,4 +1,7 @@
 import { formatCurrency } from '../../utils/helpers';
+import { useDispatch } from 'react-redux';
+import { addItem } from '../cart/cartSlice';
+
 import Button from '../../ui/Button';
 
 /**
@@ -9,6 +12,21 @@ import Button from '../../ui/Button';
  */
 function MenuItem({ pizza }) {
   const { id, name, unitPrice, ingredients, soldOut, imageUrl } = pizza;
+  const dispatch = useDispatch();
+
+  /**
+   * Add pizza to the cart
+   */
+  function handleAddToCart() {
+    const newItem = {
+      pizzaId: id,
+      name,
+      quantity: 1,
+      unitPrice,
+      totalPrice: unitPrice * 1,
+    };
+    dispatch(addItem(newItem));
+  }
 
   return (
     <li className="flex gap-4 py-2">
@@ -31,7 +49,11 @@ function MenuItem({ pizza }) {
             </p>
           )}
 
-          <Button type="small">Add to cart</Button>
+          {!soldOut && (
+            <Button type="small" onClick={handleAddToCart}>
+              Add to cart
+            </Button>
+          )}
         </div>
       </div>
     </li>
